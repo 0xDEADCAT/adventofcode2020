57d62e5a9ce9c5a4a5174d48dc5985928cf576d2
Dawid Osuchowski <dwdosuchowski@gmail.com>
2020-12-03 16:30:20 +0100

    Fix indentation in day_2/main.py

--- day_2/main.py
+++ day_2/main.py
@@ -16,25 +16,25 @@ valid2 = 0
 
 # Read numbers from file to numbers list
 with open(input_file) as f:
-   for line in f:
-    min = int(get_min.search(line).group())
-    max = int(get_max.search(line).group())
-    char = get_char.search(line).group()
-    password = get_password.search(line).group()
+    for line in f:
+        min = int(get_min.search(line).group())
+        max = int(get_max.search(line).group())
+        char = get_char.search(line).group()
+        password = get_password.search(line).group()
 
-    # Part 1
-    occurrences = password.count(char)
-    if occurrences >= min and occurrences <= max:
-        valid += 1
-    # Part 2
-    char_appears = 0
-    indexes = [i+1 for i, ch in enumerate(password) if ch == char]
-    if min in indexes:
-        char_appears += 1
-    if max in indexes:
-        char_appears += 1
-    if char_appears == 1:
-        valid2 += 1
+        # Part 1
+        occurrences = password.count(char)
+        if occurrences >= min and occurrences <= max:
+            valid += 1
+        # Part 2
+        char_appears = 0
+        indexes = [i+1 for i, ch in enumerate(password) if ch == char]
+        if min in indexes:
+            char_appears += 1
+        if max in indexes:
+            char_appears += 1
+        if char_appears == 1:
+            valid2 += 1
 
 print(valid)
 print(valid2)
