1b7a7682304ab496374fa7a48f0e38dc11fb5571
Dawid Osuchowski <dwdosuchowski@gmail.com>
2020-12-06 20:12:49 +0100

    Day 6: Part 2

--- day_6/main.py
+++ day_6/main.py
@@ -2,17 +2,20 @@
 
 input_file = "input.txt"
 
-sum = 0
-answers = set()
+_sum = 0
 
 with open(input_file) as f:
-    for line in f:
-        line = line.strip()
-        if not line:
-            sum += len(answers)
-            answers = set()
-        else:
-            for char in line:
-                answers.add(char)
+    groups = f.read().split('\n\n')
+    for group in groups:
+        group_answers = {}
+        for answers in group.splitlines():
+            for answer in answers:
+                if answer in group_answers:
+                    group_answers[answer] += 1
+                else:
+                    group_answers[answer] = 1
+        num_of_persons = len(group.splitlines())
+        _sum += sum(value == num_of_persons
+                    for value in group_answers.values())
 
-print(sum)
+print(_sum)
